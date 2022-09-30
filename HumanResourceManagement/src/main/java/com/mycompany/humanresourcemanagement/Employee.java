318e75a1fd56725f3c079488c3e27155469912fd
bhargavi <bhargavi@10.0.0.215>
2022-09-29 23:22:16 -0400

    Added comments

--- HumanResourceManagement/src/main/java/com/mycompany/humanresourcemanagement/Employee.java
+++ HumanResourceManagement/src/main/java/com/mycompany/humanresourcemanagement/Employee.java
@@ -3,8 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.humanresourcemanagement;
-
-import java.io.*;
+// importing
+import java.io.*; 
 /**
  *
  * @author bhargavi
