60ef6e5d160829694d15e856f66091845967298f
Venkata-Bhargavi <sikhakolli.v@northeastern.edu>
2022-09-30 17:35:08 -0400

    Added packages and model files

---
+++ EmployeeManagement/src/model/EmployeeProfileHistory.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author bhargavi
+ */
+public class EmployeeProfileHistory {
+    private ArrayList<EmployeeProfile> profileHistory;
+    public EmployeeProfileHistory(){
+        this.profileHistory = new ArrayList<EmployeeProfile>();
+    }
+
+    public ArrayList<EmployeeProfile> getProfileHistory() {
+        return profileHistory;
+    }
+
+    public void setProfileHistory(ArrayList<EmployeeProfile> profileHistory) {
+        this.profileHistory = profileHistory;
+    }
+    
+}
